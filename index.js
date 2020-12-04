097719ba13a99ffe9dec721bbfdada26c7289843
mw18 <67554339+mw18@users.noreply.github.com>
2020-12-04 12:09:06 -0600

    create new files

---
+++ index.js
@@ -0,0 +1,26 @@
+// array of questions for user
+
+//Please enter the name of the application
+//please describe what the project does
+// please enter name of contributing creators 
+// please enter git hub user name 
+//please enter your github email
+
+
+
+
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
