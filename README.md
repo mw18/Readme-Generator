cd954380e3e25ce801174ea91f98494746e192f4
mw18 <67554339+mw18@users.noreply.github.com>
2020-12-04 21:36:32 -0600

    add array of questions and readme generator

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# readMegenerator-
-Command-line application that dynamically generates a professional README.md file from a user's input.
+
+  #Title:
+  project 
+  
+  ## Description: 
+  explain 
+  
+  ## Table of Contents: 
+  undefined
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contributing)
+  [Credits](#credits)
+  [Tests](#test)
+  [Github](#github)
+  [Email](#email)
+
+  ## Installation: 
+  ydtjtydjt 
+
+  ## Useage: 
+  juhkhk
+
+  ## License: 
+  ;ou;uio;iu;'
+
+  ## Contributing: 
+  'o;pu;o;
+  
+  ## Credits: 
+  p;op;ip;
+
+  ## Tests: 
+  o;op;oi
+
+  ## Questions: 
+  GitHub username:mw18
+  
+  
--- index.js
+++ index.js
@@ -1,26 +1,106 @@
-// array of questions for user
+var inquirer = require('inquirer');
+const fs = require('fs');
 
-//Please enter the name of the application
-//please describe what the project does
-// please enter name of contributing creators 
-// please enter git hub user name 
-//please enter your github email
+inquirer
+  .prompt([
+      // array of questions for user
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project/app?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe the project and explain why the project exists.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please describe how to install the app by providing step by step series of examples and explanations about how to get a development running.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots if needed.',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'A short snippet describing the license (MIT, Apache etc)',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe and show how to run the tests with code examples.',
+      },
+      {
+        type: 'list',
+        name: 'questions',
+        message: 'If you have questions please contact me:',
+        choices: ['GitHub username:mw18', 'GitHub profile link: https://github.com/mw18', 'For other questions please email: melissaward@gmail.com', 'no questions at this time'],
+      },
+  ])
 
+  .then((response) => {
+    console.log('response', response);
+    
+    const userInput = `
+  
+  # Title:
+  ${response.title} 
+  
+  ## Description: 
+  ${response.description} 
+  
+  ## Table of Contents: 
+  ${response.tableOfcontents}
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contributing)
+  [Credits](#credits)
+  [Tests](#test)
+  [Github](#github)
+  [Email](#email)
 
+  ## Installation: 
+  ${response.installation} 
 
+  ## Useage: 
+  ${response.usage}
 
-const questions = [
+  ## License: 
+  ${response.license}
 
-];
+  ## Contributing: 
+  ${response.contributing}
+  
+  ## Credits: 
+  ${response.credits}
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+  ## Tests: 
+  ${response.tests}
 
-// function to initialize program
-function init() {
+  ## Questions: 
+  ${response.questions}
+  
+  `;
 
-}
+    fs.writeFile('README.md', userInput, (err) => {
+        if (err) throw err;
+        console.log ('Your answers are saved')
 
-// function call to initialize program
-init();
+});
+
+})
