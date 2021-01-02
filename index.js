8b0a2cd502e35538d0b00545bc5f347701dc8377
mw18 <67554339+mw18@users.noreply.github.com>
2021-01-02 17:16:21 -0600

    include markdown function and undate where readme file is added

--- index.js
+++ index.js
@@ -1,118 +1,102 @@
-var inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const markdownModule = require('./utils/generateMarkdown');
 const fs = require('fs');
 
-inquirer
-  .prompt([
-      // array of questions for user
+const questions =[
+
+   // array of questions for user
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project?',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Describe the project and explain why the project exists.',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install the project?',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide instructions for use.',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Select the license below',
-        choices: [ 'MIT','Other', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'Unlicense']
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Add guidelines for how other can contribute to the project.',
-      },
-      {
-        type: 'input',
-        name: 'credits',
-        message: 'List your collaborators, if any, with links to their GitHub profiles.',
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'Describe and show how to run the tests with code examples.',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'GitHub username:',
-      },
-      {
-        type: 'input',
-        name: 'giturl',
-        message: 'GitHub profile link',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'email:',
-      },
-      
-  ])
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the project?',
+    },
+   {
+    type: 'input',
+    name: 'description',
+      message: 'Describe the project and explain why the project exists.',
+   },
+   {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install the project?',
+   },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions for use.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Select the license below',
+      choices: [ 'MIT','Other', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'Unlicense']
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Add guidelines for how others can contribute to the project.',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'List your collaborators, if any, with links to their GitHub profiles.',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Describe and show how to run the tests with code examples.',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'giturl',
+      message: 'GitHub profile link',
+    },
+    {
+      type: 'input',
+      name: 'url',
+      message: 'What is the URL? of the deployed project?',
+    },
 
-  .then((response) => {
-    console.log('response', response);
-    
-    const userInput = `
-  
-  # Title:  ${response.title}
-  
-  ## Description: 
-  ${response.description} 
-  
-  ## Table of Contents: 
-  [Installation](#installation)|
-  [Usage](#usage)|
-  [License](#license)|
-  [Contributing](#contributing)|
-  [Credits](#credits)|
-  [Tests](#tests)|
-  [Github](#github)|
-  [Giturl](#giturl)|
-  [Email](#email)|
+    {
+      type: 'input',
+      name: 'email',
+      message: 'email:',
+    },
+ ];
 
-  ## Installation: 
-  ${response.installation} 
+ // function to write README file
+const writeToFile = (fileName, data) => {
+  fs.writeFile(fileName + ".md", markdownModule(data), function(err) {
+    if(err) { 
+      console.log(err); 
+    }
+    else {
+      console.log("NEW.md file had been created. Success!");
+    }
+  })
+}
 
-  ## Useage: 
-  ${response.usage}
+// function to initialize program
+const init = () => {
+  inquirer.prompt(questions)
+  .then(responses =>
+    {
+      writeToFile('NEW', responses);
+    })
+    .catch(error => {
+      if(err) {
+        console.log(err);
+      } else {
+        // Something else when wrong
+        console.log(err);
+      }
+    });
+}
 
-  ## License: 
-  ${response.license}
-
-  ## Contributing: 
-  ${response.contributing}
-  
-  ## Credits: 
-  ${response.credits}
-
-  ## Tests: 
-  ${response.tests}
-
-  ## Questions: 
-  - Github username: ${response.github}
-  - GitHub profile link: ${response.giturl}
-  - Email: ${response.email}
-  
-  `;
-
-    fs.writeFile('README2.md', userInput, (err) => {
-        if (err) throw err;
-        console.log ('Your answers are saved')
-
-});
-
-})
+// function call to start program
+init();
